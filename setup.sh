50e16367b955fb4ce7e03f36507145b27c13f341
oz <recursivefaith@pm.me>
2025-03-18 17:42:08 -0700

    250318 Add --needed flag to pacman commands and include jq, unzip, tmux packages

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@
 # - swaps CAPSLOCK with ESC
 
 # Install all packages at once
-sudo pacman -S iwd vi nano git less xdotool nodejs npm ttf-liberation noto-fonts noto-fonts-emoji ttf-dejavu pango rofi rofimoji flameshot brightnessctl redshift
+sudo pacman -S --needed iwd vi nano git less xdotool nodejs npm ttf-liberation noto-fonts noto-fonts-emoji ttf-dejavu pango rofi rofimoji flameshot brightnessctl redshift jq unzip tmux
 
 # Configure git
 git config --global init.defaultBranch main
