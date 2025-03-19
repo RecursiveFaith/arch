8f26ae18594f0c081f3bcc78c5c4cb50879c0857
oz <recursivefaith@pm.me>
2025-03-18 17:05:59 -0700

    250318 Add tmux to package list and implement setup script for git configuration

--- setup.sh
+++ setup.sh
@@ -5,8 +5,17 @@
 # ---
 # This file partially automates the instructions in README.md
 # Features:
+# - Install packages
+# - Configure git (current user only)
 # - swaps CAPSLOCK with ESC
 
+# Install all packages at once
+sudo pacman -S iwd vi nano git less xdotool nodejs npm ttf-liberation noto-fonts noto-fonts-emoji ttf-dejavu pango rofi rofimoji flameshot brightnessctl redshift
+
+# Configure git
+git config --global init.defaultBranch main
+
+# Swap CAPSLOCK with ESC
 touch /etc/udev/rules.d/99-keyboard-remap.rules
 chmod +x /etc/udev/rules.d/99-keyboard-remap.rules
 sudo tee /etc/udev/rules.d/99-keyboard-remap.rules << 'EOF'
